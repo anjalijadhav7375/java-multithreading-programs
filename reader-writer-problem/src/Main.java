3312bdef217156d980724aa5f582046f3a1c8856
Dell <anjalijadhav753@gmail.com>
2023-02-21 19:39:27 +0530

    Add reader-writer-problem-module

--- reader-writer-problem/src/Main.java
+++ reader-writer-problem/src/Main.java
@@ -1,3 +1,11 @@
+/**
+ * A CPU can be dual or quad-core and each can run one thread.
+ * Each core has its own cache and this cache is used by the thread that is running
+ * in this core.
+ * core1 --> Thread1 [core1 cache1]  <-- [Heap(Main Memory)]
+ * core2 --> Thread2 [core2 cache2]  <-- [Heap(Main Memory)]
+ * Each thread reads message object from heap and stores them into their cache.
+ */
 class Message {
     String msg;
     boolean empty;
